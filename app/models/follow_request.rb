76048ba7753c26f617b4a65db9c2d857a50e2227
nathan-frost <164823855+nathan-frost@users.noreply.github.com>
2024-05-08 22:45:19 +0000

    updated associations

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -20,6 +20,8 @@ class FollowRequest < ApplicationRecord
   # Association accessor methods to define:
   
   ## Direct associations
+  belongs_to(:sender, class_name: "User", foreign_key: "sender_id")
+  belongs_to(:recipient, class_name: "User", foreign_key: "recipient_id")
 
   # FollowRequest#sender: returns a row from the users table associated to this follow request by the sender_id column
 
--- app/models/like.rb
+++ app/models/like.rb
@@ -19,9 +19,9 @@ class Like < ApplicationRecord
   # Association accessor methods to define:
   
   ## Direct associations
-
+  belongs_to(:fan, class_name: "User", foreign_key: "fan_id")
   # Like#fan: returns a row from the users table associated to this like by the fan_id column
-
+  belongs_to(:photo, class_name: "Photo", foreign_key: "photo_id")
   # Like#photo: returns a row from the photo table associated to this like by the photo_id column
 
   def fan
